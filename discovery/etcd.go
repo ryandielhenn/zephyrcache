238435612dba8e56056327be2ec312baeaa7adf3
Ryan Dielhenn <dielhennr@gmail.com>
2025-08-14 01:04:04 -0700

    etcd-client-method-and-todo-stubs

--- discovery/etcd.go
+++ discovery/etcd.go
@@ -1,2 +1,21 @@
-//TODO etcd-backed discovery
+// TODO etcd-backed discovery
 package discovery
+
+import (
+	"time"
+
+	"go.etcd.io/etcd/client/v3"
+)
+
+func NewClient(endpoints []string) (*clientv3.Client, error) {
+	return clientv3.New(clientv3.Config{
+		Endpoints: endpoints,
+		DialTimeout: 5 * time.Second,
+	})
+}
+
+//TODO func RegisterNode
+
+//TODO func GetPeers
+
+//TODO func WatchPeers
