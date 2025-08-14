161ca0785612eb78ba444ac1972b4467e025cb8b
Ryan Dielhenn <dielhennr@gmail.com>
2025-08-14 03:02:02 -0700

    register-node-with-lease

--- discovery/etcd.go
+++ discovery/etcd.go
@@ -2,6 +2,8 @@
 package discovery
 
 import (
+	"context"
+	"fmt"
 	"time"
 
 	"go.etcd.io/etcd/client/v3"
@@ -15,6 +17,21 @@ func NewClient(endpoints []string) (*clientv3.Client, error) {
 }
 
 //TODO func RegisterNode
+func RegisterNode(cli *clientv3.Client, id, addr string, ttl int64) (clientv3.LeaseID, error) {
+	lease, err := cli.Grant(context.TODO(), ttl)
+	if err != nil {
+		return 0, err
+	}
+	key := fmt.Sprintf("/zephyr/nodes/%s", id)
+    _, err = cli.Put(context.TODO(), key, addr, clientv3.WithLease(lease.ID))
+    if err != nil {
+        return 0, err
+    }
+
+    go cli.KeepAlive(context.TODO(), lease.ID)
+
+    return lease.ID, nil
+}
 
 //TODO func GetPeers
 
